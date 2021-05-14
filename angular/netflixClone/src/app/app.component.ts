af04497979470dd36455a8ebb740d8b91afc1585
omerfarukdogann <omerfarukdogan@outlook.com.tr>
2021-05-14 13:22:24 +0300

    .

--- angular/netflixClone/src/app/app.component.ts
+++ angular/netflixClone/src/app/app.component.ts
@@ -12,5 +12,20 @@ import { ComponentFooterComponent} from 'src/app/component-footer/component-foot
 })
 export class AppComponent {
   title = 'netflixClone';
-  logo:string = "/src/includes/images/logo.png";
+  logo  = "../assets/images/logo.png";
+  coverAvengers  = "../assets/images/movies/cover-avengers.jpg";
+  coverBatman  = "../assets/images/movies/cover-batman.jpg";
+  coverDeadpool  = "../assets/images/movies/cover-deadpool.jpg";
+  coverEsaretinBedeli  = "../assets/images/movies/cover-esaretin-bedeli.jpg";
+  coverFightClub  = "../assets/images/movies/cover-fight-club.jpg";
+  coverForrestGump  = "../assets/images/movies/cover-forrest-gump.jpg";
+  coverParasite  = "../assets/images/movies/cover-gisaengchung.jpg";
+  coverGodfather  = "../assets/images/movies/cover-godfather.jpg";
+  coverGreenMile  = "../assets/images/movies/cover-green-mile.jpg";
+  coverInception  = "../assets/images/movies/cover-inception.jpg";
+  coverInterstellar  = "../assets/images/movies/cover-interstellar.jpg";
+  coverJoker  = "../assets/images/movies/cover-joker.jpg";
+  coverLeon  = "../assets/images/movies/cover-leon.jpg";
+  coverMatrix  = "../assets/images/movies/cover-matrix.jpg";
+  coverSpiderman  = "../assets/images/movies/cover-spiderman.jpg";
 }
